3e5cdd592eb322f6afbd7385a3380edefb4c0b8f
Rounak Banerjee <rounakbanerjee2.2005@gmail.com>
2026-02-19 04:48:47 +0530

    Modify dept table entries with new values
    Updated department entries in the dept table.

--- Assignment-3/3_deptTable.sql
+++ Assignment-3/3_deptTable.sql
@@ -3,9 +3,8 @@ CREATE TABLE dept (
     dept_name VARCHAR(15)
 );
 DESC TABLE dept;
-INSERT INTO dept VALUES (101, 'ComputerScience');
-INSERT INTO dept VALUES (102, 'Electrical');
-INSERT INTO dept VALUES (103, 'Mechanical');
-INSERT INTO dept VALUES (104, 'Civil');
-INSERT INTO dept VALUES (105, 'Chemical');
+INSERT INTO dept VALUES (90, 'ComputerScience');
+INSERT INTO dept VALUES (69, 'Electrical');
+INSERT INTO dept VALUES (100, 'Mechanical');
+INSERT INTO dept VALUES (110, 'Civil');
 SELECT * FROM dept;
