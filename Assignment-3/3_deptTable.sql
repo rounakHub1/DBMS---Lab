a949aa975d32a7ae10f4b2476344f0daac334e46
rounakHub1 <rounakbanerjee2.2005@gmail.com>
2026-02-19 04:42:08 +0530

    first commit

---
+++ Assignment-3/3_deptTable.sql
@@ -0,0 +1,12 @@
+DROP TABLE dept;
+CREATE TABLE dept (
+	dept_id INT,
+    dept_name VARCHAR(15)
+);
+DESC TABLE dept;
+INSERT INTO dept VALUES (101, 'ComputerScience');
+INSERT INTO dept VALUES (102, 'Electrical');
+INSERT INTO dept VALUES (103, 'Mechanical');
+INSERT INTO dept VALUES (104, 'Civil');
+INSERT INTO dept VALUES (105, 'Chemical');
+SELECT * FROM dept;
